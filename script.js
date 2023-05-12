73e2140886558ef7486dae9f86bc3cf82cc802c2
SabaMaghradze <sabamaghradze1001@gmail.com>
2023-05-12 18:38:20 +0400

    Whole project before final changes

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function convertPxToRem(pxValue) {
-    const baseFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
-    const remValue = pxValue / baseFontSize;
-    return remValue.toFixed(2) + 'rem';
-}
 
-// Get all elements in the HTML file
-const elements = document.querySelectorAll('*');
 
-// Loop through each element
-for (let i = 0; i < elements.length; i++) {
-    const element = elements[i];
 
-    // Get the element's font size in pixels
-    const fontSizePx = parseFloat(getComputedStyle(element).fontSize);
-
-    // Convert the pixel value to rem and update the element's style
-    element.style.fontSize = convertPxToRem(fontSizePx);
-}
