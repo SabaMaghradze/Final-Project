9160be41243b8bb09f6e6e4687632b12b7e1d2dd
SabaMaghradze <sabamaghradze1001@gmail.com>
2023-05-13 02:21:11 +0400

    Complete except responsive bars menu

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// const toggleBtn = document.querySelector(".fa-bars");
+// const navMenu = document.querySelector("nav ul");
+// const undoBtn = document.createElement("i");
+// undoBtn.className = "fa-solid fa-undo";
+// undoBtn.style.display = "none";
+// toggleBtn.parentNode.replaceChild(undoBtn, toggleBtn);
+
+// function toggleNav() {
+//   navMenu.classList.toggle("active");
+//   undoBtn.style.display = undoBtn.style.display === "none" ? "block" : "none";
+// }
+
+// toggleBtn.addEventListener("click", toggleNav);
+// undoBtn.addEventListener("click", toggleNav);
+
 
 
 
